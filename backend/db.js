709f18a32611c2b8e58b7e52b4da4e69de9c7e7f
jameslee <jungjo4247@naver.com>
2023-06-16 16:19:02 +0900

    feat: travis ci deploy added

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,17 @@
 const mysql = require("mysql");
+// const pool = mysql.createPool({
+//     connectionLimit: 10,
+//     host: 'mysql',
+//     user: 'root',
+//     password: 'james',
+//     database: 'myapp'
+// });
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: 'mysql',
-    user: 'root',
-    password: 'james',
-    database: 'myapp'
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT
 });
 exports.pool = pool;
